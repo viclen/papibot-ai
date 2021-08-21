cc2d2a2769fd16000e167a4f6904b40cfbcc4f1a
viclen <victorpbi5@hotmail.com>
2021-08-21 16:27:40 -0300

    feat: chat

--- DialoGPT/papibot/main_runner.py
+++ DialoGPT/papibot/main_runner.py
@@ -14,15 +14,16 @@ from transformers import (
     AutoTokenizer,
 )
 
+
 class Args():
     def __init__(self):
-        self.output_dir = 'output-small'
+        self.output_dir = 'output-medium'
         self.model_type = 'gpt2'
-        self.model_name_or_path = 'microsoft/DialoGPT-small'
-        self.config_name = 'microsoft/DialoGPT-small'
-        self.tokenizer_name = 'microsoft/DialoGPT-small'
+        self.model_name_or_path = 'microsoft/DialoGPT-medium'
+        self.config_name = 'microsoft/DialoGPT-medium'
+        self.tokenizer_name = 'microsoft/DialoGPT-medium'
         self.cache_dir = 'cached'
-        self.block_size = 32
+        self.block_size = 62
         self.do_train = True
         self.do_eval = True
         self.evaluate_during_training = False
@@ -50,12 +51,10 @@ class Args():
         self.fp16_opt_level = 'O1'
 
 
-args = Args()
+default_args = Args()
 
 
-def main(df_trn, df_val):
-    args = Args()
-
+def main(df_trn, df_val, args=default_args):
     if args.should_continue:
         sorted_checkpoints = _sorted_checkpoints(args)
         if len(sorted_checkpoints) == 0:
